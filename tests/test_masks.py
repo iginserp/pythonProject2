9fa33325f4123907e15d4d7e35eb65860ca8f357
SIginashev <sergey.iginashev@gmail.com>
2024-11-30 20:34:32 +0330

    HW 10.1 corrections

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,31 @@
-def get_mask_card_number(card: str) -> str:
+import pytest
 
-    '''Функция маскировки номера карты'''
+from src.masks import get_mask_card_number, get_mask_account
 
-    return card[0:3] + " " + card[4:5] + "** **** " + card[-4:]
+@pytest.mark.parametrize(
+    "card_number, mask_number",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("6831982476737658", "6831 98** **** 7658"),
+        ("8990922113665229", "8990 92** **** 5229"),
+        ("5999414228426353", "5999 41** **** 6353"),
+    ],
+)
+def test_mask_card_number(card_number: str, mask_number: str) -> None:
+    """Функция передает строку с номером карты"""
+    assert get_mask_card_number(card_number) == mask_number
 
 
-def get_mask_account(account: str) -> str:
-    '''Функция маскировки номера счета'''
-    return "**" + account[-4:]
+@pytest.mark.parametrize(
+    "acc_number, mask_bank_account",
+    [
+        ("73654108430135874305", "**4305"),
+        ("64686473678894779589", "**9589"),
+        ("35383033474447895560", "**5560"),
+        ("73654108430135874305", "**4305"),
+    ],
+)
+def test_mask_account(acc_number: str, mask_bank_account: str) -> None:
+    """Функция передает строку с номером счета"""
+    assert get_mask_account(acc_number) == mask_bank_account
